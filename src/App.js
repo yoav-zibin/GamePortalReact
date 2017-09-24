8cd6e91f867fd94ed7f04eab8bc6d21e6591e2d5
Simranjyot Gill <ssg441@nyu.edu>
2017-09-24 02:59:59 -0400

    adds component for header, footer and login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div>
+            <Header/>
+            <Login />
+            <Footer/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    )
   }
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <h1> Footer </h1>
+    );
+  }
+}
+
+export default Footer;
