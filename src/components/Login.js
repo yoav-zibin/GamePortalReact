13b01795f7c5cefcdf74e55793530208f013bd20
Simranjyot Gill <ssg441@nyu.edu>
2017-09-24 17:27:52 -0400

    init login page template and css, also corrects readme names

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
+import './css/Login.css';
 
 export default class Login extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="container">
+        <form className="form-signin">
+          <h2 className="form-signin-heading">
+            Please sign in
+          </h2>
+          <label for="inputEmail" className="sr-only">Email address</label>
+          <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus/>
+          <label for="inputPassword" className="sr-only">Password</label>
+          <input type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
+          <div className="checkbox">
+            <label>
+              <input type="checkbox" value="remember-me"/> Remember me
+            </label>
+          </div>
+          <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        </form>
+    </div>
     );
   }
 }
