a45f1bb3e3cdceb28de13e7954306b87b0c94156
Simranjyot Gill <ssg441@nyu.edu>
2017-10-27 04:48:23 -0400

    cleans canvas code

---
+++ src/components/CanvasImage.js
@@ -0,0 +1,24 @@
+import { Image } from 'react-konva';
+import React from 'react';
+
+// try drag& drop rectangle
+class CanvasImage extends React.Component {
+  state = {
+    image: null
+  };
+  componentDidMount() {
+    const image = new window.Image();
+    image.src = this.props.src;
+    image.onload = () => {
+      this.setState({
+        image: image
+      });
+    };
+  }
+
+  render() {
+    return <Image ref="image" {...this.props} image={this.state.image} />;
+  }
+}
+
+export default CanvasImage;
--- src/components/PlayArena.js
+++ src/components/PlayArena.js
@@ -1,40 +1,21 @@
 import React, { Component } from 'react';
 import { Layer, Stage, Image } from 'react-konva';
+import CanvasImage from './CanvasImage';
 
 export default class PlayArena extends Component {
-  constructor(){
-      super();
-      this.state = {
-          image : null
-      };
-  }
-
-  componentDidMount() {
-    const image = new window.Image();
-    const piece = new window.Image();
-    image.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Chessboard480.svg/1200px-Chessboard480.svg.png';
-    piece.src = 'https://www.shareicon.net/data/128x128/2016/07/20/799015_chess_512x512.png';
-    image.onload = () => {
-        this.setState({
-          image: image
-        });
-    }
-    piece.onload = () => {
-        this.setState({
-          piece: piece
-        });
-    }
-  }
-
   render() {
+    //Temp variables
+    var board = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Chessboard480.svg/1200px-Chessboard480.svg.png';
+    var piece = 'https://www.shareicon.net/data/128x128/2016/07/20/799015_chess_512x512.png';
+
     return (
     <div>
       <Stage width={400} height={400}>
         <Layer>
-          <Image ref='image' height={400} width={400} image={this.state.image} />
+          <CanvasImage height={400} width={400} src={board} />
         </Layer>
         <Layer>
-            <Image ref='image' draggable={true} height={50} width={50} image={this.state.piece} />
+            <CanvasImage ref='image' draggable={true} height={50} width={50} src={piece} />
         </Layer>
       </Stage>
     </div>
