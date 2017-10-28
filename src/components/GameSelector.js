726e759fe3ebd1bf3fba49767d17d0ab7ca6793c
Simranjyot Gill <ssg441@nyu.edu>
2017-10-28 18:01:12 -0400

    implements play-arena "mostly"

---
+++ src/components/GameSelector.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+import Select from 'react-select';
+import './css/GameSelector.css';
+import {db} from '../firebase';
+
+export default class GameSelector extends React.Component {
+
+  constructor(){
+      super();
+      this.state = {
+          specs:null,
+          selectedSpec: null
+      };
+  }
+
+  loadGame(event){
+      if(this.state.selectedSpec){
+          this.props.setSpec(this.state.selectedSpec.value);
+      }else{
+          // TODO:
+          // Show some error message
+      }
+  }
+
+  componentDidMount(){
+      this.loadSpecs();
+  }
+
+  loadSpecs(){
+      var specRef = db.ref('gameBuilder/gameSpecs');
+      var self = this;
+      specRef.on("value",function(snapshot){
+          var specs = snapshot.val();
+          var list = [];
+          for(var specKey in specs){
+              list.push({
+                value: specs[specKey],
+                label: specs[specKey].gameName
+            });
+          }
+        self.setState({specs:list});
+      })
+  }
+
+  changeSelectedSpec(event){
+      this.setState({
+          selectedSpec: event
+      });
+  }
+
+  render() {
+    return (
+        <div className='game-selector-container'>
+            <Select
+              className="spec-selector"
+              name="form-field-name"
+              value={this.state.selectedSpec}
+              options={this.state.specs}
+              onChange={this.changeSelectedSpec.bind(this)}
+            />
+            <Button color="success load-game-btn" onClick={this.loadGame.bind(this)}>Load Game</Button>
+        </div>
+    );
+  }
+}
