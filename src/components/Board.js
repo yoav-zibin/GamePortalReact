c1cc20bd5e9b114645eddd664f607d8811351714
Simranjyot Gill <ssg441@nyu.edu>
2017-10-28 05:58:34 -0400

    saving work

---
+++ src/components/Board.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Layer, Stage, Image } from 'react-konva';
+import CanvasImage from './CanvasImage';
+
+export default class Board extends Component {
+  render() {
+    //Temp variables
+    var board = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Chessboard480.svg/1200px-Chessboard480.svg.png';
+    var piece = 'https://www.shareicon.net/data/128x128/2016/07/20/799015_chess_512x512.png';
+
+    return (
+    <div>
+      <Stage width={400} height={400}>
+        <Layer>
+          <CanvasImage height={400} width={400} src={board} />
+        </Layer>
+        <Layer>
+            <CanvasImage ref='image' draggable={true} height={50} width={50} src={piece} />
+        </Layer>
+      </Stage>
+    </div>
+    );
+  }
+}
--- src/components/PlayArena.js
+++ src/components/PlayArena.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react';
-import { Layer, Stage, Image } from 'react-konva';
-import CanvasImage from './CanvasImage';
+import Board from './Board';
 
 export default class PlayArena extends Component {
   render() {
-    //Temp variables
-    var board = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Chessboard480.svg/1200px-Chessboard480.svg.png';
-    var piece = 'https://www.shareicon.net/data/128x128/2016/07/20/799015_chess_512x512.png';
 
     return (
     <div>
-      <Stage width={400} height={400}>
-        <Layer>
-          <CanvasImage height={400} width={400} src={board} />
-        </Layer>
-        <Layer>
-            <CanvasImage ref='image' draggable={true} height={50} width={50} src={piece} />
-        </Layer>
-      </Stage>
+      <Board/>
     </div>
     );
   }
