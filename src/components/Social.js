c211d56b4fb5b4323b3c6ca41841049ad7a72cd6
Simranjyot Gill <ssg441@nyu.edu>
2017-11-16 00:20:21 -0500

    don't allow user to login with same provider again

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,16 +4,29 @@ import { SocialIcon } from 'react-social-icons';
 import {auth, facebookProvider, githubProvider, googleProvider, twitterProvider} from '../firebase';
 
 export default class Social extends React.Component{
+
     handleSocialConnect(provider){
         let self = this;
-        auth.currentUser.linkWithPopup(facebookProvider).then(function(result) {
+        let providerId = provider.providerId;
+        let providerExists = false;
+        auth.currentUser.providerData.forEach((myProvider)=>{
+            if(providerExists)
+                return;
+            if(providerId === myProvider.providerId)
+                providerExists = true;
+        });
+        if(providerExists){
+            self.props.success();
+            return;
+        }
+        auth.currentUser.linkWithPopup(provider).then(function(result) {
             // Accounts successfully linked.
             var credential = result.credential;
             var user = result.user;
-            // ...
+
             self.props.success();
         }).catch(function(error) {
-            console.log('error linking facebook account');
+            console.log('error linking account:'+error.message);
             self.props.failed();
         });
     }
@@ -22,19 +35,19 @@ export default class Social extends React.Component{
         return(
             <div className="social-inner-container">
                 <SocialIcon
-                    onClick={this.handleSocialConnect.bind(this, twitterProvider)}
+                    onClick={()=>{this.handleSocialConnect(twitterProvider)}}
                     className='social-icon'
                     network="twitter"/>
                 <SocialIcon
-                    onClick={this.handleSocialConnect.bind(this, facebookProvider)}
+                    onClick={()=>{this.handleSocialConnect(facebookProvider)}}
                     className='social-icon'
                     network="facebook"/>
                 <SocialIcon
-                    onClick={this.handleSocialConnect.bind(this, googleProvider)}
+                    onClick={()=>{this.handleSocialConnect(googleProvider)}}
                     className='social-icon'
                     network="google"/>
                 <SocialIcon
-                    onClick={this.handleSocialConnect.bind(this, githubProvider)}
+                    onClick={()=>{this.handleSocialConnect(githubProvider)}}
                     className='social-icon'
                     network="github"/>
             </div>
