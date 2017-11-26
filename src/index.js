f135dcd352a8cf20b1784efcf9604516e7768c90
Simranjyot Gill <ssg441@nyu.edu>
2017-11-26 16:10:55 -0500

    manual way to append firebase service worker code to service worker auto generated by react build system

--- src/index.js
+++ src/index.js
@@ -4,27 +4,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-import {messaging, auth, initPushNotification} from './firebase';
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register(process.env.PUBLIC_URL + '/firebase-messaging-sw.js')
-    .then((registration) => {
-        // console.log(registration.scope);
-        messaging.useServiceWorker(registration);
-        auth.onAuthStateChanged(user => {
-          if (user) {
-            initPushNotification();
-          }
-        });
-    }).catch((err) => {
-        console.log('Unable to register Service Worker:', err);
-    })
-}
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
   <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
 ), document.getElementById('root'));
-// registerServiceWorker();
+registerServiceWorker();
